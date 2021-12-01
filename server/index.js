2b85ba45002aaa2d5f521a4fb195394429e052e3
nilsv01 <nils.vogelpohl1@gmail.com>
2021-12-01 18:15:06 +0100

    final

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(4000,() =>{
+    console.log("server is running")
+});
 
 const db = mysql.createConnection({
     user: 'root',
@@ -14,6 +20,13 @@ const db = mysql.createConnection({
     insecureAuth : true
 });
 
+db.connect(err =>{
+    if(err) {
+        return err;
+    }
+});
+console.log.connection;
+
 app.post('/add',(req, res) => {
 
     db.query('UPDATE visitor SET visitorMoment = visitorMoment + 1, visitorDaily = visitorDaily + 1, visitorTotal = visitorTotal + 1 WHERE id = 1',
@@ -21,10 +34,18 @@ app.post('/add',(req, res) => {
         if(err) {
             console.log(err);
         } else {
-   //*         console.log("new Visitor");
+          console.log("new Visitor");
         }
     }
     );
+    db.query("SELECT * FROM visitor", (err,result) => {
+        if(err){
+            console.log(err);
+        }else{
+    //*     console.log(JSON.parse(JSON.stringify(result))[0].visitorMoment);
+            res.send(result);
+        }      
+   });
 });
 
 app.post('/sub',(req, res) => {
@@ -34,10 +55,18 @@ app.post('/sub',(req, res) => {
         if(err) {
             console.log(err);
         } else {
-   //*         console.log("left Visitor");
+            console.log("left Visitor");
         }
     }
     );
+    db.query("SELECT visitorMoment FROM visitor", (err,result) => {
+        if(err){
+            console.log(err);
+        }else{
+    //*     console.log(JSON.parse(JSON.stringify(result))[0].visitorMoment);
+            res.send(result);
+        }      
+   });
 });
 
 app.post('/resMoment',(req, res) => {
@@ -47,10 +76,18 @@ app.post('/resMoment',(req, res) => {
         if(err) {
             console.log(err);
         } else {
-   //*         console.log("new Moment");
+            console.log("new Moment");
         }
     }
     );
+    db.query("SELECT visitorMoment FROM visitor", (err,result) => {
+        if(err){
+            console.log(err);
+        }else{
+    //*     console.log(JSON.parse(JSON.stringify(result))[0].visitorMoment);
+            res.send(result);
+        }      
+   });
 });
 
 app.post('/resDaily',(req, res) => {
@@ -60,10 +97,18 @@ app.post('/resDaily',(req, res) => {
         if(err) {
             console.log(err);
         } else {
-   //*         console.log("new Day");
+            console.log("new Day");
         }
     }
     );
+    db.query("SELECT visitorDaily FROM visitor", (err,result) => {
+        if(err){
+            console.log(err);
+        }else{
+    //*     console.log(JSON.parse(JSON.stringify(result))[0].visitorMoment);
+            res.send(result);
+        }      
+   });
 });
 
 app.post('/resTotal',(req, res) => {
@@ -73,10 +118,18 @@ app.post('/resTotal',(req, res) => {
         if(err) {
             console.log(err);
         } else {
-    //*        console.log("new Total");
+            console.log("new Total");
         }
     }
     );
+    db.query("SELECT visitorTotal FROM visitor", (err,result) => {
+        if(err){
+            console.log(err);
+        }else{
+    //*     console.log(JSON.parse(JSON.stringify(result))[0].visitorMoment);
+            res.send(result);
+        }      
+   });
 });
 
 app.get('/getVisitorMoment', (req, res) => {
@@ -110,6 +163,3 @@ app.get('/getVisitorTotal', (req, res) => {
  });
 });
 
-app.listen(3001,() =>{
-    console.log("server is running")
-});
